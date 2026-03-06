dbb0087df3cbaba98e5cd177fb7e4c1bf700490f
landysh <landyshajrullina089@gmail.com>
2026-03-07 01:46:38 +0300

    Добавлена кнопка и окно с информацией об авторе

---
+++ Group4338/4338_Khayrullina.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Group4338
+{
+    public partial class _4338_Khayrullina : Window
+    {
+        public _4338_Khayrullina()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Group4338/MainWindow.xaml.cs
+++ Group4338/MainWindow.xaml.cs
@@ -5,6 +5,14 @@ namespace Group4338
     public partial class MainWindow : Window
     {
         public MainWindow()
-            => InitializeComponent();
+        {
+            InitializeComponent();
+        }
+
+        private void KhayrullinaButton_Click(object sender, RoutedEventArgs e)
+        {
+            var authorWindow = new _4338_Khayrullina();
+            authorWindow.ShowDialog();
+        }
     }
 }
